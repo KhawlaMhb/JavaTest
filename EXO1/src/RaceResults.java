fae381fd83eb26f9184d1ea515011e21bccee8a4
khawla <hamza.achahour@gmail.com>
2020-03-11 15:57:02 +0100

    last version

--- EXO1/src/RaceResults.java
+++ EXO1/src/RaceResults.java
@@ -1,33 +1,30 @@
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RaceResults {
 	
-	Map<String,TimeDuration> res;
+	Map<String,Integer> res;
 	
 	public RaceResults() {
-		res =  new HashMap<String,TimeDuration>(); 
+		res =  new HashMap<String,Integer>(); 
 	}
 	
 	public void onNewResult(String tagNumber, TimeDuration resultTime) {
-		res.put(tagNumber, resultTime);
+		res.put(tagNumber, resultTime.getDuration());
 	}
 	
 	public void printResults() throws BadBadValueException {
-		List<Integer> sorted = new ArrayList<Integer>();
+		final Map<String, Integer> sortedByCount = res.entrySet()
+                .stream()
+                .sorted((Map.Entry.<String, Integer>comparingByValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+		for (Map.Entry<String,Integer> entry : sortedByCount.entrySet())
+			System.out.println(entry.getKey()  +" : " + new TimeDuration(entry.getValue()));
 		
-		for (Map.Entry<String,TimeDuration> entry : res.entrySet())
-		{ 
-			sorted.add(entry.getValue().getDuration());
-		}
-		Collections.sort(sorted);
-		for(Integer i: sorted) {
-			System.out.println(new TimeDuration(i).toString());
-		}
 	}
 
 }
+
